ee0bdc4e780cf46559e8edc31a6a2cbb7bfa7b36
Benjamin Spiegel <bspiegel100@gmail.com>
2022-04-09 19:39:24 -0500

    Refactor

--- wezterm/.config/wezterm/lua/rose-pine.lua
+++ wezterm/.config/wezterm/lua/rose-pine.lua
@@ -2,7 +2,7 @@
 
 local M = {}
 
-local rose_pine = {
+local palette = {
     base = '#191724',
     overlay = '#26233a',
     muted = '#6e6a86',
@@ -10,33 +10,33 @@ local rose_pine = {
 }
 
 local active_tab = {
-    bg_color = rose_pine.overlay,
-    fg_color = rose_pine.text,
+    bg_color = palette.overlay,
+    fg_color = palette.text,
 }
 
 local inactive_tab = {
-    bg_color = rose_pine.base,
-    fg_color = rose_pine.muted,
+    bg_color = palette.base,
+    fg_color = palette.muted,
 }
 
 function M.colors()
     return {
         tab_bar = {
-            background = rose_pine.base,
+            background = palette.base,
             active_tab = active_tab,
             inactive_tab = inactive_tab,
             inactive_tab_hover = active_tab,
             new_tab = inactive_tab,
             new_tab_hover = active_tab,
-            inactive_tab_edge = rose_pine.muted, -- (Fancy tab bar only)
-        }
+            inactive_tab_edge = palette.muted, -- (Fancy tab bar only)
+        },
     }
 end
 
 function M.window_frame() -- (Fancy tab bar only)
     return {
-        active_titlebar_bg = rose_pine.base,
-        inactive_titlebar_bg = rose_pine.base,
+        active_titlebar_bg = palette.base,
+        inactive_titlebar_bg = palette.base,
     }
 end
 
--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
-local wezterm = require 'wezterm';
+-- local wezterm = require 'wezterm';
 
-local colors = require 'rose-pine'.colors()
-local window_frame = require 'rose-pine'.window_frame()
+local colors = require 'lua/rose-pine'.colors()
+local window_frame = require 'lua/rose-pine'.window_frame()
 
 -- WezTerm config
 
@@ -9,73 +9,21 @@ return {
     hide_tab_bar_if_only_one_tab = true,
     use_fancy_tab_bar = false,
     -- enable_scroll_bar = true,
-    
     line_height = 1.1,
-
     -- font = wezterm.font("Victor Mono", {weight="Medium"}),
     -- font = wezterm.font("Fira Code"),
-   
     font_size = 13,
-
-    color_scheme = "rose-pine",
-    -- color_scheme = "nord",
-    -- TODO: duplicate the nord color scheme and make these changes:
-    -- selection_bg = "#424C5d",
-    -- selection_fg = "#d8dee8",
-    
-    -- Note: right-side padding is also the width of the scroll bar
+    color_scheme = 'rose-pine',
     window_padding = {
         left = 5,
-        right = 5,
+        right = 5, -- also the width of the scroll bar
         top = 2,
         bottom = 2,
     },
-
     colors = colors,
-
     window_frame = window_frame,
-    
-    -- nord
-    --[[ colors = {
-        tab_bar = {
-            -- Color of the tab strip along the top of the window
-            background = "#2e3440",
-
-            -- Colors for the active tab 
-            active_tab = {
-                -- The color of the background area for the tab
-                bg_color = "#eceff4",
-                -- The color of the text for the tab
-                fg_color = "#4c566a",
-            },
-
-            -- Colors for inactive tabs 
-            inactive_tab = {
-                bg_color = "#2e3440",
-                fg_color = "#d8dee9",
-            },
-
-            -- Hover color for inactive tabs
-            inactive_tab_hover = {
-                bg_color = "#424C5D",
-                fg_color = "#d8dee9",
-                italic = false,
-            },
-
-            -- Colors for the new tab button (same as inactive tabs)
-            new_tab = {
-                bg_color = "#2e3440",
-                fg_color = "#d8dee9",
-            },
-
-            --Hover colors for the new tab button (same as inactive tabs)
-            new_tab_hover = {
-                bg_color = "#424C5D",
-                fg_color = "#d8dee9",
-                italic = false,
-            }
-        }
+    --[[ keys = {
+        {key="L", mods="CTRL", action="ShowDebugOverlay"},
+        {key="R", mods="CTRL", action="ReloadConfiguration"}
     } ]]
 }
-
-
