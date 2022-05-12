bc5b1e145a610aff690b0d5c6aa2efc05bf8c9d6
Benjamin Spiegel <bspiegel100@gmail.com>
2022-05-11 23:29:52 -0500

    Update completion mappings

--- nvim/.config/nvim/lua/plugins/config/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/config/nvim-cmp.lua
@@ -2,15 +2,29 @@
 -- plugins/config/nvim-cmp.lua - config for nvim-cmp plugin
 --
 
+-- local has_words_before = function()
+--     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+--     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+-- end
+
 local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+
+-- Add () when inserting a function with completion.
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on( 'confirm_done', cmp_autopairs.on_confirm_done({  map_char = { tex = '' } }))
 
 local snippet = {
     expand = function(args)
-        require 'luasnip'.lsp_expand(args.body)
+        luasnip.lsp_expand(args.body)
     end,
 }
 
 local mapping = {
+    -- Ctrl+k and Ctrl+j select items. Arrows also work.
+    ['<C-k>'] = cmp.mapping.select_prev_item(),
+    ['<C-j>'] = cmp.mapping.select_next_item(),
+
     -- Ctrl+b and Ctrl+f scroll the docs window.
     ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), { 'i', 'c' }),
     ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
@@ -27,24 +41,33 @@ local mapping = {
         c = cmp.mapping.close(),
     }),
 
-    -- Either Return or Tab confirms the first suggestion.
-    -- ['<CR>'] = cmp.mapping(cmp.mapping.confirm({ select = true }), { 'i', 'c' }),
-    ['<Tab>'] = cmp.mapping(cmp.mapping.confirm({ select = true }), { 'i', 'c' }),
+    -- Return confirms the first suggestion.
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
 
-    -- Alternative mapping for Tab (from cmp wiki IntelliJ-style snippet):
-    -- Don't select anything automatically. Tab selects then confirms.
-    --[[ ["<Tab>"] = cmp.mapping(function(fallback)
+    -- Tab confirms the first suggestion or jumps into a snippet.
+    ['<Tab>'] = cmp.mapping(function(fallback)
         if cmp.visible() then
-            local entry = cmp.get_selected_entry()
-            if not entry then
-                cmp.select_next_item({ behavior = cmp.SelectBehavior.Select })
-            else
-                cmp.confirm()
-            end
+            -- cmp.select_next_item()
+            cmp.confirm({ select = true })
+        elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+        -- elseif has_words_before() then
+        --     cmp.complete()
         else
             fallback()
         end
-    end, {"i","s","c",}), ]]
+    end, { "i", "s" }),
+
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+        -- if cmp.visible() then
+        --      cmp.select_prev_item()
+        --  elseif luasnip.jumpable(-1) then
+        if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+        else
+            fallback()
+        end
+    end, { "i", "s" }),
 }
 
 cmp.setup {
@@ -58,6 +81,9 @@ cmp.setup {
         { name = 'path' },
         { name = 'calc' },
     }),
+    experimental = {
+       ghost_text = true,
+    }
 }
 
 cmp.setup.cmdline(':', {
