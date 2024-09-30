b37b4f14fabaecbe9e86bc2f66c34ab35c86dc4d
Benjamin Spiegel <bspiegel100@gmail.com>
2024-09-30 17:12:39 -0500

    neovim: mini.completion - auto-brackets and tweaks

--- nvim/.config/nvim/lua/plugins/config/mini/completion.lua
+++ nvim/.config/nvim/lua/plugins/config/mini/completion.lua
@@ -1,5 +1,12 @@
 --
--- plugins/config/mini/completion.lua - config for mini.completion plugin
+-- plugins/config/mini/completion.lua - tweaks to built-in completion
+--     for use with mini.completion and mini.pairs.
+--
+-- Notes:
+--   - This file doesn't require mini.completion. It also affects the
+--     built-in completion (<C-x><C-o>).
+--   - Only one line requires mini.pairs (with an alternative for stock).
+--   - Load after mini.pairs (and any other plugin that remaps <CR>).
 --
 
 require("mini.completion").setup {
@@ -8,17 +15,15 @@ require("mini.completion").setup {
 }
 
 -- To use `<Tab>` and `<S-Tab>` for navigation through completion list
-
-local imap_expr = function(lhs, rhs)
-    vim.keymap.set("i", lhs, rhs, { expr = true })
-end
-imap_expr("<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
-imap_expr("<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
+-- local imap_expr = function(lhs, rhs)
+--     vim.keymap.set("i", lhs, rhs, { expr = true })
+-- end
+-- imap_expr("<Tab>", [[pumvisible() ? "\<C-n>" : "\<Tab>"]])
+-- imap_expr("<S-Tab>", [[pumvisible() ? "\<C-p>" : "\<S-Tab>"]])
 
 -- To get more consistent behavior of `<CR>`. With the list visible and
--- an item selected (i.e., completed in the text), it should confirm
--- and close, not add a new line.
-
+-- an item selected (i.e., completed in the text), confirm and close.
+-- Without this, it confirms and closes and also adds a new line.
 local keycode = vim.keycode
     or function(x)
         return vim.api.nvim_replace_termcodes(x, true, true, true)
@@ -35,12 +40,32 @@ _G.cr_action = function()
         local item_selected = vim.fn.complete_info()["selected"] ~= -1
         return item_selected and keys["ctrl-y"] or keys["ctrl-y_cr"]
     else
-        -- If popup is not visible, use plain `<CR>`. You might want to customize
-        -- according to other plugins. For example, to use 'mini.pairs', replace
-        -- next line with `return require('mini.pairs').cr()`
+        -- If popup is not visible, use plain `<CR>`.
         -- return keys["cr"]
-        return require('mini.pairs').cr()
+        -- Or use mini.pairs function instead of plain `<CR>`.
+        return require("mini.pairs").cr()
     end
 end
 
 vim.keymap.set("i", "<CR>", "v:lua._G.cr_action()", { expr = true })
+
+-- Auto-insert () and put the cursor in the middle after completing a function.
+-- Credit to LazyVim (auto_brackets function in util.cmp.lua)
+local function auto_brackets()
+    local completed_item_kind = vim.v.completed_item.kind
+    if not completed_item_kind then return end
+
+    if completed_item_kind == "Function" then
+        local cursor = vim.api.nvim_win_get_cursor(0)
+        local prev_char = vim.api.nvim_buf_get_text(0, cursor[1] - 1, cursor[2], cursor[1] - 1, cursor[2] + 1, {})[1]
+        if prev_char ~= "(" and prev_char ~= ")" then
+            local keys_to_feed = vim.api.nvim_replace_termcodes("()<left>", false, false, true)
+            vim.api.nvim_feedkeys(keys_to_feed, "i", true)
+        end
+    end
+end
+
+vim.api.nvim_create_autocmd({ "CompleteDonePre" }, {
+    pattern = "*",
+    callback = function() auto_brackets() end,
+})
